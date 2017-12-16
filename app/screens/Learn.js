574071604304255d94057f5a9fb0a4cb5841b779
eroltutumlu <eroltutumlu@gmail.com>
2017-12-16 22:01:46 +0300

    Icon files added, learn screen design changed

--- app/screens/Learn.js
+++ app/screens/Learn.js
@@ -2,42 +2,124 @@ import React, {Component} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { colors } from '../config/styles';
 import Button from '../components/Button';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: colors.background,
+      alignSelf:'stretch',
+      backgroundColor: colors.backgroundColor
     },
-    main: {
-      fontSize: 20,
-      textAlign: 'center',
-      color: colors.headerText,
-      fontWeight: '400',
-      fontStyle: 'italic',
+    innerContainer:{
+      flex:1,
+      alignSelf:'stretch',
+      margin: 10,
+      padding:5,
+      backgroundColor:'#FFF'
     },
+    content:{
+      flex: 1,
+      flexDirection:'column',
+      justifyContent:'space-around',
+      alignSelf: 'stretch'
+    },
+    textSection:{
+      flex:4,
+    },
+    characterSection:{
+      flex: 2,
+      borderRadius: 4, borderWidth: 2.5, borderColor: '#77D400',
+      justifyContent:'center', alignItems:'center',
+      marginBottom: 7,
+      backgroundColor:'#FFF'
+    },
+    morseSection:{
+      flex: 2,
+      justifyContent:'center', alignItems:'center',
+      borderRadius: 4, borderWidth: 2.5, borderColor: '#77D400',
+      marginBottom: 7,
+      backgroundColor:'#FFF'
+    },
+    buttonSection:{
+      flex: 2,
+      alignSelf:'stretch',
+      borderRadius: 6, borderWidth: 2.5, borderColor: '#77D400',
+      backgroundColor:'#FFF'
+    },
+    header:{
+      flexDirection:'row',
+      marginBottom:7
+    },
+    iconSection:{
+      flex:2,
+      justifyContent:'center',
+      alignItems:'center'
+    },
+    learnText:{
+      fontWeight:'bold', fontSize:18, color:'#000'
+    },
+    astText:{
+      padding:5, color:'#fff',borderRadius: 6, alignSelf: 'flex-start',backgroundColor:'#A9A9A9'
+    },
+    letter:{fontSize:50, fontWeight:'500'},
+    answer:{fontSize:35, fontWeight:'500'},
+    userInput:{fontSize:54, fontWeight:'500'},
+    buttonContainer:{flex:1, backgroundColor:'#77D400'},
+    button: {flex:1, justifyContent:'center', alignItems:'center'}
 });
 
 class Learn extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
   render() {
     return (
         <View style={styles.container}>
-          <View style={{backgroundColor:'#F5F2F9', flex:1}}>
-            <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-              <Text style={{fontSize:55}}>A</Text>
-              <TouchableOpacity>
-                <Text style={{fontSize:25}}>?</Text>
-              </TouchableOpacity>
+          <View style={styles.innerContainer}>
+            <View style={styles.content}>
+              <View style={styles.header}>
+                <View style={styles.textSection}>
+                  <Text style={styles.learnText}>Learn using Koch Method</Text>
+                  <Text style={styles.astText}>Complete the assesment</Text>
+                </View>
+                <View style={styles.iconSection}>
+                  <Icon name="assessment" size={50} color="#86DF13" />
+                </View>
+              </View>
+           
+              <View style={styles.characterSection}>
+                <Text style={styles.letter}>B</Text>
+                <TouchableOpacity>
+                  <Icon name="help" size={36} color="#757575" />
+                </TouchableOpacity>
+                <Text style={styles.answer}></Text>
+              </View>
+              <View style={styles.morseSection}>
+                <Text style={styles.userInput}>-...</Text>
+              </View>
+              <View style={styles.buttonSection}>
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity activeOpacity={0.6} style={styles.button} 
+                      onPressIn = {
+                        () => {
+                          console.warn('In')
+                        }
+                      } 
+                      onPressOut = {
+                        () => {
+                          console.warn('In')
+                        }
+                      }
+                    >  
+                
+                    <Icon name="refresh" size={60} color="#fff" />
+
+                  </TouchableOpacity>
+                </View>
+              </View>
             </View>
-          </View>
-          <View style={{backgroundColor:'#e3eddb', flex:1, justifyContent:'center', alignItems:'center'}}>
-          
-          <Text style={{fontSize:50}}>-.--</Text>
-          </View>
-          <View style={{backgroundColor:'#91ba6f', flex:1}}>
-            <TouchableOpacity style={{backgroundColor:'#6a9844', borderRadius:3, alignItems:'center', justifyContent:'center', flex:1, margin:25}} activeOpacity={0.4}>
-              <Text style={{color:'#fff', fontSize:26, fontWeight:'500'}}>Press</Text>
-            </TouchableOpacity>
           </View>
         </View>
       );
